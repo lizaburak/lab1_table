6fc55a8fdf8c8f32efc3e24ce535f1459104d0bb
Liza_Burak <b.a.z.i.liza007@gmail.com>
2021-09-26 21:35:51 +0300

    feat: add code in main.js file

---
+++ main.js
@@ -0,0 +1,376 @@
+const data = [
+    {
+      "Id": "247d7907-df87-5174-b08c-a39f8d32d23a",
+      "productName": "Practical Concrete Gloves",
+      "quantity": 94,
+      "price": 822,
+      "producer": "Becker Inc",
+      "cайт": "https://larue.com",
+      "кассир": "Beaulah",
+      "manager": "Sylvester"
+    },
+    {
+      "Id": "24f870ce-667c-545b-b770-ee4a0d858bb3",
+      "productName": "Rustic Wooden Car",
+      "quantity": 63,
+      "price": 485,
+      "producer": "Kutch - Ankunding",
+      "cайт": "https://araceli.info",
+      "кассир": "Evangeline",
+      "manager": "Sylvester"
+    },
+    {
+      "Id": "40b7a3b2-b119-5ce2-9068-5558bb7507ca",
+      "productName": "Incredible Steel Fish",
+      "quantity": 98,
+      "price": 157,
+      "producer": "Rowe, Legros and Marquardt",
+      "cайт": "https://concepcion.name",
+      "кассир": "Nya",
+      "manager": "Jenifer"
+    },
+    {
+      "Id": "68fb9a12-c787-548e-a238-8c3c2b948046",
+      "productName": "Generic Fresh Chips",
+      "quantity": 97,
+      "price": 622,
+      "producer": "Tillman LLC",
+      "cайт": "http://nelle.name",
+      "кассир": "Kenna",
+      "manager": "Jenifer"
+    },
+    {
+      "Id": "1d7ae150-c5c2-530c-83d1-9730f16c3d42",
+      "productName": "Fantastic Metal Soap",
+      "quantity": 7,
+      "price": 515,
+      "producer": "Thompson, Mayert and Hirthe",
+      "cайт": "http://rhoda.biz",
+      "кассир": "Jamal",
+      "manager": "Sylvester"
+    },
+    {
+      "Id": "95908830-3828-5f8d-98ed-b8ab235246ff",
+      "productName": "Handcrafted Soft Tuna",
+      "quantity": 61,
+      "price": 636,
+      "producer": "Welch, Corkery and Nicolas",
+      "cайт": "https://burnice.org",
+      "кассир": "Johnny",
+      "manager": "Ericka"
+    },
+    {
+      "Id": "b30eff71-09f8-5457-ae63-deab767ee25e",
+      "productName": "Handcrafted Steel Keyboard",
+      "quantity": 52,
+      "price": 841,
+      "producer": "Lynch - Crist",
+      "cайт": "https://velma.com",
+      "кассир": "Madonna",
+      "manager": "Ericka"
+    },
+    {
+      "Id": "2740dd8e-af5d-5529-95b4-0c6597a15036",
+      "productName": "Generic Wooden Table",
+      "quantity": 53,
+      "price": 535,
+      "producer": "Ledner Group",
+      "cайт": "https://preston.name",
+      "кассир": "Coleman",
+      "manager": "Jenifer"
+    },
+    {
+      "Id": "b28768d6-a390-50da-b4a7-50767ee5d378",
+      "productName": "Practical Wooden Chair",
+      "quantity": 35,
+      "price": 869,
+      "producer": "Hettinger LLC",
+      "cайт": "http://robb.org",
+      "кассир": "Toby",
+      "manager": "Sylvester"
+    },
+    {
+      "Id": "212e1458-641b-5d02-893e-e10e06e4f20b",
+      "productName": "Handcrafted Cotton Chair",
+      "quantity": 74,
+      "price": 260,
+      "producer": "Howell, Kunze and Gutkowski",
+      "cайт": "https://kaleb.info",
+      "кассир": "Matilde",
+      "manager": "Sylvester"
+    },
+    {
+      "Id": "bfcd0f99-f07b-5a5b-968a-8af71aed68ed",
+      "productName": "Practical Soft Shirt",
+      "quantity": 28,
+      "price": 942,
+      "producer": "Schimmel - Torp",
+      "cайт": "http://mossie.name",
+      "кассир": "Michaela",
+      "manager": "Will"
+    },
+    {
+      "Id": "d389772e-2faf-51d9-b71f-b389e15a2c95",
+      "productName": "Licensed Wooden Sausages",
+      "quantity": 84,
+      "price": 304,
+      "producer": "Mueller Inc",
+      "cайт": "https://dean.com",
+      "кассир": "Dock",
+      "manager": "Ericka"
+    },
+    {
+      "Id": "a183665e-e12e-5699-8eed-b06565b7e43f",
+      "productName": "Tasty Frozen Fish",
+      "quantity": 33,
+      "price": 100,
+      "producer": "Koelpin and Sons",
+      "cайт": "https://antone.biz",
+      "кассир": "Marilie",
+      "manager": "Ericka"
+    },
+    {
+      "Id": "f7988aa8-a3a2-50c6-bce2-2524e8a832f2",
+      "productName": "Intelligent Steel Bacon",
+      "quantity": 64,
+      "price": 718,
+      "producer": "Bradtke Inc",
+      "cайт": "https://lysanne.net",
+      "кассир": "Carolanne",
+      "manager": "Ericka"
+    },
+    {
+      "Id": "7955f016-8844-545b-8d54-eb5ec44ebaba",
+      "productName": "Incredible Cotton Mouse",
+      "quantity": 66,
+      "price": 684,
+      "producer": "Wisozk Group",
+      "cайт": "http://sister.biz",
+      "кассир": "Rose",
+      "manager": "Ericka"
+    },
+    {
+      "Id": "f46dfc68-1771-5742-8593-428788900666",
+      "productName": "Gorgeous Steel Tuna",
+      "quantity": 60,
+      "price": 751,
+      "producer": "Marvin, Rogahn and Satterfield",
+      "cайт": "https://selena.name",
+      "кассир": "Mustafa",
+      "manager": "Jenifer"
+    },
+    {
+      "Id": "3587041a-abb0-5d1d-a66f-b156e1dcdaa7",
+      "productName": "Awesome Steel Chips",
+      "quantity": 57,
+      "price": 111,
+      "producer": "Moore - Borer",
+      "cайт": "https://kiera.name",
+      "кассир": "Eloisa",
+      "manager": "Sylvester"
+    },
+    {
+      "Id": "f76770e2-3ae2-5074-aee3-fc2cd73315a6",
+      "productName": "Unbranded Plastic Soap",
+      "quantity": 47,
+      "price": 534,
+      "producer": "Powlowski Group",
+      "cайт": "http://jessyca.biz",
+      "кассир": "Edgar",
+      "manager": "Ericka"
+    },
+    {
+      "Id": "318d6b1a-10ab-523b-aa00-f25c04ff9a78",
+      "productName": "Fantastic Fresh Sausages",
+      "quantity": 80,
+      "price": 1000,
+      "producer": "Becker - Hegmann",
+      "cайт": "http://jeffry.name",
+      "кассир": "Jeramie",
+      "manager": "Ericka"
+    },
+    {
+      "Id": "0b9cd2ed-440d-52f0-bcab-9185af66af08",
+      "productName": "Unbranded Frozen Gloves",
+      "quantity": 73,
+      "price": 210,
+      "producer": "Weber - Von",
+      "cайт": "http://orie.net",
+      "кассир": "Rosalia",
+      "manager": "Jenifer"
+    },
+    {
+      "Id": "65ff6be3-0a95-5872-b66d-6a995e2c9b5d",
+      "productName": "Sleek Wooden Shoes",
+      "quantity": 86,
+      "price": 291,
+      "producer": "Powlowski - Ullrich",
+      "cайт": "http://mireya.info",
+      "кассир": "Alessandra",
+      "manager": "Jenifer"
+    },
+    {
+      "Id": "ffb73c9b-07cc-55da-a153-b3c3d9c5f026",
+      "productName": "Licensed Frozen Tuna",
+      "quantity": 40,
+      "price": 898,
+      "producer": "Rowe - Fadel",
+      "cайт": "http://liana.info",
+      "кассир": "Vallie",
+      "manager": "Jenifer"
+    },
+    {
+      "Id": "deb26db5-6c8a-521c-adf3-289b46d64756",
+      "productName": "Incredible Frozen Towels",
+      "quantity": 52,
+      "price": 876,
+      "producer": "Hyatt - Rempel",
+      "cайт": "https://dagmar.com",
+      "кассир": "Jadon",
+      "manager": "Ericka"
+    },
+    {
+      "Id": "5aacc128-2b61-5b04-b78b-bf7fd2ac2dc6",
+      "productName": "Incredible Concrete Sausages",
+      "quantity": 93,
+      "price": 260,
+      "producer": "Franecki LLC",
+      "cайт": "https://evans.com",
+      "кассир": "Eleanora",
+      "manager": "Will"
+    },
+    {
+      "Id": "2c9c428b-b9f6-56bc-b555-75a5dadca33b",
+      "productName": "Handmade Soft Pants",
+      "quantity": 22,
+      "price": 599,
+      "producer": "Connelly, Marks and O'Connell",
+      "cайт": "https://kiel.net",
+      "кассир": "Caden",
+      "manager": "Will"
+    },
+    {
+      "Id": "3367ca75-c1a5-5c0a-9ef7-3711b10e5e9d",
+      "productName": "Handcrafted Wooden Ball",
+      "quantity": 84,
+      "price": 57,
+      "producer": "Gulgowski, Dach and Oberbrunner",
+      "cайт": "https://herta.biz",
+      "кассир": "Aaliyah",
+      "manager": "Ericka"
+    },
+    {
+      "Id": "098c72ff-c95e-535c-9cb1-79cfe702359b",
+      "productName": "Unbranded Cotton Towels",
+      "quantity": 86,
+      "price": 897,
+      "producer": "MacGyver Group",
+      "cайт": "http://cordelia.info",
+      "кассир": "Bernita",
+      "manager": "Ericka"
+    },
+    {
+      "Id": "3f0d8b2d-6944-5ef6-8911-bce188439cd4",
+      "productName": "Practical Wooden Computer",
+      "quantity": 22,
+      "price": 863,
+      "producer": "Ziemann, Wunsch and Ryan",
+      "cайт": "https://aubree.info",
+      "кассир": "Kailee",
+      "manager": "Ericka"
+    },
+  ];
+
+let counter = 0;
+
+function makeTable() {
+    let table = document.createElement("table");
+    const body = document.querySelector("body");
+    body.append(table);
+    table.classList.add("body-table");
+    data.forEach(element => {
+      tr = document.createElement("tr");
+      table.append(tr);
+      let lastRow = document.querySelectorAll("tr")[document.querySelectorAll("tr").length - 1]; 
+      let dataValue = Object.values(element);
+      counter++;
+      dataValue.forEach(dataElement => {
+        let td = document.createElement("td");
+        lastRow.append(td);
+        td.innerHTML = dataElement;
+      })
+    });
+    const headerTr = document.createElement("tr");
+    table.prepend(headerTr);
+    for (let name in data[0]) {
+      let td = document.createElement("td");
+      headerTr.append(td);
+      td.textContent = name;
+    }
+  }
+  makeTable();
+
+
+  function countTable() {
+    const body = document.querySelector("body");
+    let table = document.createElement("table");
+    body.append(table);
+    table.classList.add("body-tableCount");
+    let tr = document.createElement("tr");
+    table.append(tr);
+    let td = document.createElement("td");
+    tr.append(td);
+    td.innerHTML = "Total count of product";
+    td = document.createElement("td");
+    tr.append(td);
+    td.innerHTML = counter-1;
+    };
+    countTable();
+
+
+
+    function makeManagersTable() {
+        let managersObject = {};
+        const body = document.querySelector("body");
+        let table = document.createElement("table");
+        body.append(table);
+        table.classList.add("body-tableCount");
+        let tr = document.createElement("tr");
+        table.append(tr);
+        let td = document.createElement("td");
+        tr.append(td);
+        td = document.createElement("td");
+        tr.append(td);
+        td.textContent = "Total quantity";
+        td = document.createElement("td");
+        tr.append(td);
+        td.textContent = "Total price";
+      
+        data.forEach(e => {
+          if (managersObject[e.manager] == undefined) {
+            managersObject[e.manager] = [];
+            managersObject[e.manager][0] = e.quantity;
+            managersObject[e.manager][1] = e.price;
+          } else {
+            managersObject[e.manager][0] += e.quantity;
+            managersObject[e.manager][1] += e.price;
+          }
+        });
+
+        for (let manager in managersObject) {
+          let tr = document.createElement("tr");
+          table.append(tr);
+          let td = document.createElement("td");
+          tr.append(td);
+          td.textContent = manager;
+          (function makeTd1() {
+            let td = document.createElement("td");
+            tr.append(td);
+            td.textContent = managersObject[manager][0];
+            td = document.createElement("td");
+              tr.append(td);
+              td.textContent = managersObject[manager][1];
+          })()
+        }
+      }
+      makeManagersTable();
